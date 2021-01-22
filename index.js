ef7f4aabf9577393cf02993c86c8f72ecc27dd50
Gregor Jacob <grgorjacob@gmail.com>
2021-01-22 15:03:03 +0100

    fixing

--- index.js
+++ index.js
@@ -125,5 +125,9 @@ client.on("message", message => {
     } 
 });
 
-client.login(process.env.BOT_TOKEN).catch(console.error);
+try {
+client.login(process.env.BOT_TOKEN);
+} catch (Error e) {
+	console.log("Some Error");
+}
 //client.login(token); // starts the bot up
