9b9bc3c93051f44555d9ce6841ee350880c6b83a
Gregor Jacob <grgorjacob@gmail.com>
2021-01-25 12:44:24 +0100

    db

--- db.js
+++ db.js
@@ -19,15 +19,15 @@ function getCommands(serverId) {
 	try {
 	client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   	if (err) throw err;
+  	console.log("ROWS:")
   	for (let row of res.rows) {
     	console.log(JSON.stringify(row));
   	}
-  client.end();
-});
+	});
 	} catch (error) {
 		console.log("error");
 	}
-	return info;
+	//return info;
 		
 }
 
