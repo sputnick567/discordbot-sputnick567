eb7364791e8b48e45365cd2451d6851a0d5a40f0
Gregor Jacob <grgorjacob@gmail.com>
2021-01-25 12:31:18 +0100

    db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 
 const client = new Client({
-		connectionString: conn,
+		connectionString: process.env.DATABASE_URL,
 		ssl: {
 		rejectUnauthorized: false
 		  }
