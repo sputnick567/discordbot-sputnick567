2baaaaa217b52f890fd2c27442e5e2b03602d349
Gregor Jacob <grgorjacob@gmail.com>
2021-01-25 12:25:59 +0100

    database"

--- db.js
+++ db.js
@@ -1,15 +1,21 @@
-function init(conn) {
-	const { Client } = require('pg');
 
-	const client = new Client({
-	  connectionString: conn,
-	  ssl: {
-	    rejectUnauthorized: false
-	  }
+
+
+const client;
+
+function init () {
+	const { Client } = require('pg');
+	client = new Client({
+		connectionString: conn,
+		ssl: {
+		rejectUnauthorized: false
+		  }
 	});
 
 	client.connect();
 }
+
+
 
 function getCommands(serverId) {
 	let info = client.query("SELECT EXISTS (SELECT * FROM server_info WHERE serverId = " + serverId);
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js"); // imports the discord library
 const client = new Discord.Client(); // creates a discord client
 //const token = fs.readFileSync("token.txt").toString(); // gets your token from the file
 const db = require("./db.js");
-
+db.init();
 client.once("ready", () => { // prints "Ready!" to the console once the bot is online
 	console.log("Ready!");
 });
