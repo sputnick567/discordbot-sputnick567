02112f4905f0849290ea7212b7ad034f33cb9b02
Gregor Jacob <grgorjacob@gmail.com>
2021-01-25 12:42:49 +0100

    db

--- db.js
+++ db.js
@@ -17,7 +17,13 @@ function init () {
 
 function getCommands(serverId) {
 	try {
-	let info = client.query("SELECT EXISTS (SELECT * FROM server_info WHERE serverId = " + serverId + ")");
+	client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  	if (err) throw err;
+  	for (let row of res.rows) {
+    	console.log(JSON.stringify(row));
+  	}
+  client.end();
+});
 	} catch (error) {
 		console.log("error");
 	}
